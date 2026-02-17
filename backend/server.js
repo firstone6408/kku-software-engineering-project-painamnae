ac48fbbe3c196f91234035d9a02c62c5d1ab9721
Ekkawich Pilawan <prosterz171@gmail.com>
2026-02-18 05:37:56 +0700

    sprint 1: fix: cors in server.js

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,9 @@ app.use(helmet());
 
 const corsOptions = {
     origin: ['http://localhost:3001',
-        'https://amazing-crisp-9bcb1a.netlify.app'],
+        'https://amazing-crisp-9bcb1a.netlify.app',
+        'https://sinchai-document.com',
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
